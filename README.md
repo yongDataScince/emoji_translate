229deca49e50a864335b26e9491d99000f7bce08
kirillNovoseletskii <48125494+kirillNovoseletskii@users.noreply.github.com>
2021-10-17 11:49:17 +0300

    betta

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 this is a shared repository of students from KUBSAU to work on the project. The point is to implement a translator from emoji into full sentences and words.
 
 ## Specs
-
 Language: Python
+
+## You can translate for exaample:
+- иди домой/иду домой
+- я норм/ок/хорошо/отлично
+- 
--- main.py
+++ main.py
@@ -1,4 +1,42 @@
+# import libraries for lemmatization and for read args
+from typing import List
+import pymorphy2
+import sys
+import re
+
+# Init lemmetizator
+morph = pymorphy2.MorphAnalyzer()
+
+# word to normal state
+def lemm_word(word: str) -> str:
+  return morph.parse(word)[0].normal_form
+
+def parse_args():
+  return re.sub(r'[^\w\s]', '', sys.argv[2]).split(" ")
+
+# words dictionary
+map_words : dict = {}
+
 with open("./data/emoji_dict.txt") as data:
   for l in data.readlines():
     key, value = l.split("-")
-    print(key, value)
+    map_words[key] = value.replace("\n", "")
+
+
+def translate(args):
+  ans = ""
+  for arg in args:
+    for key in map_words.keys():
+      if lemm_word(arg) in key.split("/"):
+        if "1per" in morph.parse(arg)[0].tag and arg != "я": ans += "👆🏼"
+        elif "2per" in morph.parse(arg)[0].tag and arg != "ты": ans += "👇🏻"
+        
+        ans += map_words[key] + " "
+
+        if sys.argv[2][-1] == "?" : ans += "❓"
+  return ans
+
+
+
+if sys.argv[1] == "-t":
+  print(translate(parse_args()))
