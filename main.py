96c242e1efcf531311e7c07039048f40eba7592e
kirillNovoseletskii <48125494+kirillNovoseletskii@users.noreply.github.com>
2021-10-19 08:57:22 +0300

    Revert "Merge pull request #4 from yongDataScince/k_nov"
    This reverts commit e948c9af9dc74b45bdc3230710205f62d3c50fe4, reversing
    changes made to 1e0d4356cc72d8a535b82464f5e866f650eff6fd.

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ import re
 # Init lemmetizator
 morph = pymorphy2.MorphAnalyzer()
 
-# all flags
-all_flags: List[str] = ["-t", "-h", "--help"]
-
 # word to normal state
 def lemm_word(word: str) -> str:
   return morph.parse(word)[0].normal_form
@@ -20,48 +17,17 @@ def parse_args():
 # words dictionary
 map_words : dict = {}
 
-# parsing text file
 with open("./data/emoji_dict.txt") as data:
-  """
-  here we go through each line of the file 
-  and split it into two values 
-  using the split("-") function
-  """
   for l in data.readlines():
     key, value = l.split("-")
     map_words[key] = value.replace("\n", "")
 
 
-
-def translate(args) -> str:
+def translate(args):
   ans = ""
   for arg in args:
     for key in map_words.keys():
       if lemm_word(arg) in key.split("/"):
-        # if word for 1per or 2per we're adding spec. emoji
-        if "1per" in morph.parse(arg)[0].tag and "я" not in args: ans += "👆🏼"
-        elif "2per" in morph.parse(arg)[0].tag and "ты" not in args: ans += "👇🏻"
-        
-        ans += map_words[key] + " "
-
-  # if the sentence is interrogative then add a question mark
-  if sys.argv[2][-1] == "?" : ans += "❓"
-
-  return ans
-
-
-# you can translate only if flat -t
-if sys.argv[1] not in all_flags:
-  print(f"Unexpected flag '{sys.argv[1]}'")
-
-elif sys.argv[1] == "-t":
-  print(translate(parse_args()))
-
-elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
-  print("""
-    help: '-h' or '--help'
-    translate text to emoji: '-t'
-  """)
         if "1per" in morph.parse(arg)[0].tag and arg != "я": ans += "👆🏼"
         elif "2per" in morph.parse(arg)[0].tag and arg != "ты": ans += "👇🏻"
         
